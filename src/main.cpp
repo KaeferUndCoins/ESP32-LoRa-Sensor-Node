50ae5831889a67b69e90104256d3f0e39cbf7a4c
Haustuer <haustuer@live.de>
2020-05-16 01:55:52 +0200

    Lora.endPacket() added

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 
 #include <SPI.h>
 #include <LoRa.h>
+//#include <SoftwareSerial.h>
 //#include <HardwareSerial.h> // sollte bereits mit Arduino IDE installiert sein
 //#include <SoftwareSerial.h>
 //HardwareSerial SerialGPS(2);
@@ -22,28 +23,17 @@ int test=0;
 
 void setup() {
   //initialize Serial Monitor  g
-  Serial.begin(9600);
-  Serial1.begin(9600);
-  Serial2.begin(9600);
-  
+  Serial.begin(9600);  
   //SerialGPS.begin(9600, SERIAL_8N1, 16, 17);
-
   while (!Serial);
   Serial.println("LoRa Sender");
-
   //setup LoRa transceiver module
-  LoRa.setPins(ss, rst, dio0);
-  
+  LoRa.setPins(ss, rst, dio0);  
   //replace the LoRa.begin(---E-) argument with your location's frequency 
   //433E6 for Asia
   //866E6 for Europe
   //915E6 for North America
-  pinMode(ledpin,OUTPUT);
-/*  digitalWrite(ledpin,HIGH);
-  delay(500);
- digitalWrite(ledpin,LOW);
-  delay(500);*/
-  
+  pinMode(ledpin,OUTPUT);  
   while (!LoRa.begin(868E6)) {
     Serial.print(".");
     c++;
@@ -55,48 +45,25 @@ void setup() {
   // ranges from 0-0xFF
   digitalWrite(ledpin,HIGH);
   LoRa.setSyncWord(0xF3);
-  Serial.println("LoRa Initializing OK! Neu");
+  Serial.println("LoRa Initializing OK!");
 
   pinMode(ADC_1,INPUT);
 }
 
 int incomingByte = 0; // for incoming serial data
-
-
 int sensIN;
 void loop() {
-  if (Serial1.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-
-    // say what you got:
-   Serial.print(incomingByte, DEC);
-     Serial.println(" on Serial 1");
-  }
+  
   if (Serial.available() > 0) {
     // read the incoming byte:
     incomingByte = Serial.read();
-
     // say what you got:
    Serial.print(incomingByte, DEC);
      Serial.println(" on Serial 0");
   }
-  if (Serial2.available() > 0) {
-    // read the incoming byte:
-    incomingByte = Serial.read();
-
-    // say what you got:
-    Serial.print("I received: ");
-    Serial.print(incomingByte, DEC);
-     Serial.println(" on Serial 2");
-  }
-
-
-
-
   sensIN=analogRead(ADC_1);
-  //Serial.print("Sending packet: ");
- // Serial.println(counter);
+  Serial.print("Sending packet: ");
+  Serial.println(counter);
   
   //Send LoRa packet to receiver
   LoRa.beginPacket();
@@ -104,10 +71,10 @@ void loop() {
   LoRa.print(counter);
   LoRa.print("  -   Sensor:");
   LoRa.print(sensIN);
-  //Serial.println(LoRa.endPacket());
- 
-
+  Serial.println(LoRa.endPacket());
   counter++;
-if(counter>=10000){counter=0;}
+  if(counter>=10000){
+    counter=0;
+  }
   delay(1000);
 }
